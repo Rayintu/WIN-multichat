8470a425cb2a4b1c8899d0b20926b4c950371ec8
Ricky <rickybroers.rb@gmail.com>
2019-10-02 21:05:57 +0200

    Added more file structure

---
+++ GeneralClassLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GeneralClassLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ TCPClassLibrary/RickyTcpClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCPClassLibrary
+{
+    class RickyTcpClient
+    {
+        public void listenForMessages(Action<String> message)
+        {
+
+        }
+    }
+}
